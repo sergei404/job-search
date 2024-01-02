d6eaaaf10f9c4814b31e022a319c1c5387c74ec8
Sergei <kvakazuabr@yandex.ru>
2024-01-02 23:35:39 +0300

    add tests

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,5 +1,4 @@
 import {render, screen} from '@testing-library/vue';
-
 import MainNav from '@/components/MainNav.vue';
 import { expect } from 'vitest';
 
@@ -8,6 +7,21 @@ describe("MainNav", () => {
     render(MainNav);
     const name = screen.getByText("Dodo Careers")
     expect(name).toBeInTheDocument();
+  })
+
+  it('displays menu items for navigation', () => {
+    render(MainNav);
+    const navigationList = screen.getAllByRole("listitem")
+    const navigationText = navigationList.map(el => el.textContent)
+    console.log(navigationText);
+    expect(navigationText).toEqual([
+      'Teams',
+      'Location',
+      'Life at Dodo Careers',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
   })
 })
 
